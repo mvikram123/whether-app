db582027c0c6b8c40ab2482dc1ed85cc11c6ca0c
manbodhvikram <manbodhvikram@gmail.com>
2023-08-18 12:24:08 +0530

    whether-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App=()=>{
+
+  let[lat,setLat]=useState("")
+  let[long,setLong]=useState("")
+  let[hemisphere,setHemisphere]=useState("")
+
+  function findLocation(){
+    if(navigator.geolocation){
+
+      navigator.geolocation.getCurrentPosition(
+        (position)=>{
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+
+         updateHemisphere(position.coords.latitude)
+      
+        }
+      )
+    }
+
+
+  }
+      function updateHemisphere(lat){
+
+
+        if(lat>0){
+          setHemisphere("Northern hemisphere")
+        }
+        else if(lat<0)
+        {
+          setHemisphere("Southern hemisphere")
+        }
+        else{
+          setHemisphere("equitor")
+        }
+      }
+
+  return(
+
+
+    <div>
+
+      <button onClick={findLocation}>Find MY Location</button>
+      <h1>Latitute: {lat}</h1>
+      <h1>Longitute: {long}</h1>
+      {/* {
+        lat && updateHemisphere()
+      } */}
+      <h1>Hemisphere: {hemisphere}</h1>
     </div>
-  );
+  )
 }
-
 export default App;
